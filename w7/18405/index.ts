d20da1917a53b21645eece63fc59a06a3f5bd5f4
math-hsh <104745781+math-hsh@users.noreply.github.com>
2023-04-06 21:39:08 +0900

    Create index.ts

---
+++ w7/18405/index.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from "fs";
+
+// 이게 왜 골드..?
+
+class InputHandler {
+  constructor(
+    public readonly rows: number[][],
+    public readonly s: number,
+    public readonly n: number,
+    public readonly k: number,
+    public readonly x: number,
+    public readonly y: number
+  ) {}
+
+  static fromStdin() {
+    const rawInput = readFileSync("/dev/stdin").toString().trim();
+    const rawRows = rawInput.split("\n");
+    const [n, k] = (rawRows.shift() || "").trim().split(" ").map(Number);
+    const [s, x, y] = (rawRows.pop() || "").trim().split(" ").map(Number);
+
+    const rows = rawRows.map((s) => s.split(" ").map(Number));
+
+    return new InputHandler(rows, s, n, k, x, y);
+  }
+}
+
+class ProblemResolver {
+  constructor(private readonly input: InputHandler) {}
+
+  solve() {
+    return 1;
+  }
+}
+
+console.log(new ProblemResolver(InputHandler.fromStdin()).solve());
