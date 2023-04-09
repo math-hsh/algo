2ebff06529796a87d69e84c33c8d669d5a3fcf75
math-hsh <104745781+math-hsh@users.noreply.github.com>
2023-04-09 18:13:19 +0900

    I hate ordered output

--- w7/2583/index.ts
+++ w7/2583/index.ts
@@ -23,14 +23,19 @@ class InputHandler {
   static fromStdin() {
     const rawInput = readFileSync("/dev/stdin").toString().trim();
     const [firstLine, ...restLine] = rawInput.split("\n");
-    const [m, n, k] = firstLine.split(" ").map(Number);
+    const [m, n, k] = firstLine.split(" ").map((s) => Number(s.trim()));
 
     return new InputHandler(
       m,
       n,
       k,
       restLine
-        .map((row) => row.split(" ").map(Number))
+        .map((row) =>
+          row
+            .trim()
+            .split(" ")
+            .map((s) => Number(s.trim()))
+        )
         .map(
           ([sx, sy, ex, ey]) =>
             new Rectangle({ x: sx, y: sy }, { x: ex, y: ey })
@@ -42,10 +47,15 @@ class InputHandler {
 class ProblemResolver {
   constructor(private readonly input: InputHandler) {}
 
-  solve() {
-    const blocked = Array.from({ length: this.input.n }).map(() =>
-      Array.from({ length: this.input.m }).map(() => false)
+  createMatrix() {
+    return Array.from({ length: this.input.m }).map(() =>
+      Array.from({ length: this.input.n }).map(() => false)
     );
+  }
+
+  solve() {
+    const blocked = this.createMatrix();
+    const checked = this.createMatrix();
 
     for (const rectangle of this.input.rectangles) {
       const sx = rectangle.leftTop.x;
@@ -53,15 +63,74 @@ class ProblemResolver {
       const sy = rectangle.leftTop.y;
       const ey = rectangle.rightBottom.y;
 
-      for (let x = sx; x <= ex; x++) {
-        for (let y = sy; y <= ey; y++) {
-          blocked[x][y] = true;
+      for (let x = sx; x < ex; x++) {
+        for (let y = sy; y < ey; y++) {
+          blocked[y][x] = true;
+          checked[y][x] = true;
         }
       }
     }
 
-    console.log({ blocked });
+    let count = 0;
+    const spaces: number[] = [];
+
+    const mark = (x: number, y: number, isFirst = false) => {
+      const directions = [
+        [-1, 0],
+        [0, -1],
+        [1, 0],
+        [0, 1],
+      ];
+
+      if (checked[x][y]) return;
+
+      if (isFirst) {
+        count++;
+        spaces[count - 1] ??= 1;
+      } else {
+        spaces[count - 1]++;
+      }
+
+      checked[x][y] = true;
+
+      for (const [dx, dy] of directions) {
+        if (
+          !checked[x + dx] ||
+          checked[x + dx]?.[y + dy] == undefined ||
+          checked[x + dx]?.[y + dy]
+        ) {
+          continue;
+        }
+
+        if (!blocked[x + dx]?.[y + dy]) {
+          mark(x + dx, y + dy);
+        }
+      }
+    };
+
+    for (let x = 0; x < this.input.m; x++) {
+      for (let y = 0; y < this.input.n; y++) {
+        mark(x, y, true);
+      }
+    }
+
+    return {
+      count,
+      spaces,
+    };
   }
 }
 
-console.log(new ProblemResolver(InputHandler.fromStdin()).solve());
+class OutputHandler {
+  constructor(private readonly input: InputHandler) {}
+
+  print() {
+    const answer = new ProblemResolver(this.input).solve();
+
+    console.log(
+      `${answer.count}\n` + [...answer.spaces].sort((a, b) => a - b).join(" ")
+    );
+  }
+}
+
+new OutputHandler(InputHandler.fromStdin()).print();
